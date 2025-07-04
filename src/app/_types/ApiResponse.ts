024470f29cafd112119d5041c1b917e549565188
megumegu256 <120536958+megumegu256@users.noreply.github.com>
2025-07-05 04:48:49 +0900

    delete 81 files

--- src/app/_types/ApiResponse.ts
+++
@@ -1,6 +0,0 @@
-export type ApiResponse<T> = {
-  success: boolean;
-  payload: T;
-  message: string; // 主にエラーメッセージなど
-  metadata?: string; // JSON形式のメタ情報
-};
--- src/app/_types/LoginRequest.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-import { emailSchema, passwordSchema } from "./CommonSchemas";
-
-export const loginRequestSchema = z.object({
-  email: emailSchema,
-  password: passwordSchema,
-});
-
-export type LoginRequest = z.infer<typeof loginRequestSchema>;
